7feff555af472d23a331481ef90526d1cd1fa41d
lang-sky <langsky121@gmail.com>
2023-11-01 05:59:23 -0400

    requirements, packages, router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Index } from "./pages/index";
+import { ThankYou } from "./pages/thank-you";
+import { Navigate, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { routes } from "./constants/routes";
+
+const router = createBrowserRouter([
+  {
+    path: routes.index,
+    element: <Index />,
+  },
+  {
+    path: routes.thankyou,
+    element: <ThankYou />,
+  },
+  {
+    path: "*",
+    element: <Navigate to={routes.index} />,
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
