c14177351526946fb109f5e929289c1a21f83621
lang-sky <langsky121@gmail.com>
2023-11-03 12:14:50 -0400

    confirmation warning before submit if touched

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,5 @@
 import * as yup from "yup";
-import { Form, Formik } from "formik";
+import { Form, Formik, FormikProps } from "formik";
 import { useNavigate } from "react-router-dom";
 import { DynamicLayout } from "components/DynamicLayout";
 import { Card } from "components/common";
@@ -9,7 +9,7 @@ import { SET_PERSON } from "store";
 import { routes } from "constants/routes";
 import { Field, Person } from "types";
 import { LabeledFormField } from "./components/LabeledFormField";
-import { FC } from "react";
+import { FC, useEffect, useRef } from "react";
 import * as Styled from "./index.styles";
 
 interface IndexProps {
@@ -20,8 +20,29 @@ export const Index: FC<IndexProps> = ({ fieldSet }) => {
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const { data } = useAppSelector((state) => state.person);
+  const formRef = useRef<FormikProps<Person>>(null);
 
   const validationSchema = yup.object().shape(createYupSchema(fieldSet));
+
+  useEffect(() => {
+    const showConfirmationWarning = (e: BeforeUnloadEvent) => {
+      if (!formRef.current?.touched) {
+        return;
+      }
+
+      var confirmationMessage =
+        "It looks like you have been editing something. " +
+        "If you leave before saving, your changes will be lost.";
+
+      (e || window.event).returnValue = confirmationMessage; //Gecko + IE
+      return confirmationMessage; //Gecko + Webkit, Safari, Chrome etc.
+    };
+    window.addEventListener("beforeunload", showConfirmationWarning);
+
+    return () => {
+      window.removeEventListener("beforeunload", showConfirmationWarning);
+    };
+  }, []);
 
   const handleSubmit = (values: Person) => {
     dispatch({ type: SET_PERSON, payload: values });
@@ -30,7 +51,12 @@ export const Index: FC<IndexProps> = ({ fieldSet }) => {
 
   return (
     <Card>
-      <Formik initialValues={data} validationSchema={validationSchema} onSubmit={handleSubmit}>
+      <Formik
+        initialValues={data}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+        innerRef={formRef}
+      >
         <Form>
           <DynamicLayout fieldSet={fieldSet} FieldComponent={LabeledFormField} />
           <Styled.Submit type="submit" aria-label="submit">
